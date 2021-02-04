c7bc6736bf6491a8397b4e88b719dab68bc2c8e3
9831065 <najafizadeh21@gmail.com>
2021-02-04 11:05:26 +0330

    Hashing added

---
+++ hash.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h> 
+#include <string>
+
+using namespace std; 
+const int CAPACITY = 20;
+
+class HashNode 
+{ 
+    public:
+        int free; 
+        int value; 
+        string key; 
+}; 
+
+
+HashNode memory[CAPACITY];
+
+
+void init()
+{
+    for (int i = 0; i < CAPACITY; i++)
+    {
+        memory[i].free = 1;
+        memory[i].key = "NULL";
+    }
+}
+
+
+int hash_function(string line)
+{
+    return line.length();
+}
+
+
+int key_generate(string line)
+{
+    int total_length = hash_function(line);
+    int temp = 0;
+    for (int i = 0; i < CAPACITY; i++)
+    {
+        temp = (total_length + i) % CAPACITY;
+        if (memory[temp].free == 1)
+        {
+            return temp;
+        }
+    }
+    return -1;
+}
+
+void insert(string line)
+{
+    int index = key_generate(line);
+    if (index == -1)
+    {
+        return;
+    } else 
+    {
+        memory[index].key = line;
+        memory[index].free = 0;
+        memory[index].value = 1;
+    }
+}
+
+void print()
+{
+    for (int i = 0; i < CAPACITY; i++)
+    {
+        if (memory[i].free == 1)
+        {
+            cout << " __ ";
+        } else
+        {
+            cout << " " << memory[i].key << " "; 
+        }
+        if (i == CAPACITY / 2)
+        {
+            cout << endl;
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    init();
+    insert("12+3");
+    print();
+    insert("12+32");
+    insert("1");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+34643");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    insert("12+252443");
+    print();
+    return 0;
+}
+  
